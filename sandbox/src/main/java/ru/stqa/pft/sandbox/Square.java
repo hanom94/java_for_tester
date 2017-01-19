76e6c26d6c41ab0768c29acb38f0fba6de597d8f
Viktor Yeromenko <hanom94@gmail.com>
2017-01-19 22:50:24 +0200

    Класссы, методы и функции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by hanom on 19.01.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+
+  public double area(){
+    return this.l * this.l;
+  }
+}
