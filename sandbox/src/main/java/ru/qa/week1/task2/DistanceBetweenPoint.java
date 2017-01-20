c9a9ce6db819b3a1248b66fe578ffbe015500d4e
Viktor Yeromenko <hanom94@gmail.com>
2017-01-20 17:53:18 +0200

    week1tast2

---
+++ sandbox/src/main/java/ru/qa/week1/task2/DistanceBetweenPoint.java
@@ -0,0 +1,15 @@
+package ru.qa.week1.task2;
+
+public class DistanceBetweenPoint {
+
+  public static void main(String[] args) {
+    Point p1 = new Point(7, 7);
+    Point p2 = new Point(14,14);
+    System.out.println("Расстояние между двумя точками: "
+            + p1.x + " : " + p1.y + " и " + p2.x + " : " + p2.y + " = " + distance(p1, p2));
+  }
+
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+  }
+}
---
+++ sandbox/src/main/java/ru/qa/week1/task2/Point.java
@@ -0,0 +1,14 @@
+package ru.qa.week1.task2;
+
+/**
+ * Created by hanom on 20.01.2017.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+  public Point (double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+}
