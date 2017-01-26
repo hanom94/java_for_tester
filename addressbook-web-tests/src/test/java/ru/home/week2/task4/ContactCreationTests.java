bc6b8544b475a92266fcfcdf6e8e9ad2b7e99d97
Viktor Yeromenko <hanom94@gmail.com>
2017-01-26 13:25:08 +0200

    Перенесение вспомогательного метода в TestBase

--- addressbook-web-tests/src/test/java/ru/home/week2/task4/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/home/week2/task4/ContactCreationTests.java
@@ -1,17 +1,18 @@
 package ru.home.week2.task4;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.TestBase;
 
-public class ContactCreationTests extends BaseTest {
+public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
-        initContactCreation();
-        fillContactForm(new ContactData("Виктор", "Викторович",
-                "ViktorXX", "Киев", "+380988888888",
-                "+380999999999", "viktorxx@mail.ua",
-                "Киев, Улица 1 ", "+380933333333"));
-        submitContactCreation();
-    }
+  @Test
+  public void testContactCreation() {
+    initContactCreation();
+    fillContactForm(new ContactData("Виктор", "Викторович",
+            "ViktorXX", "Киев", "+380988888888",
+            "+380999999999", "viktorxx@mail.ua",
+            "Киев, Улица 1 ", "+380933333333"));
+    submitContactCreation();
+  }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import ru.home.week2.task4.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,7 +13,7 @@ import java.util.concurrent.TimeUnit;
  * Created by hanom on 25.01.2017.
  */
 public class TestBase {
-  FirefoxDriver wd;
+  public FirefoxDriver wd;
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
       try {
@@ -80,5 +81,44 @@ public class TestBase {
 
   protected void selectGroup() {
       wd.findElement(By.name("selected[]")).click();
+  }
+
+  protected void submitContactCreation() {
+      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  protected void fillContactForm(ContactData contactData) {
+      wd.findElement(By.name("firstname")).click();
+      wd.findElement(By.name("firstname")).clear();
+      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+      wd.findElement(By.name("lastname")).click();
+      wd.findElement(By.name("lastname")).clear();
+      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+      wd.findElement(By.name("nickname")).click();
+      wd.findElement(By.name("nickname")).clear();
+      wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
+      wd.findElement(By.name("address")).click();
+      wd.findElement(By.name("address")).clear();
+      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+      wd.findElement(By.name("home")).click();
+      wd.findElement(By.name("home")).clear();
+      wd.findElement(By.name("home")).sendKeys(contactData.getHomeTelephone());
+      wd.findElement(By.name("mobile")).click();
+      wd.findElement(By.name("mobile")).clear();
+      wd.findElement(By.name("mobile")).sendKeys(contactData.getMobileTelephone());
+      wd.findElement(By.name("email")).click();
+      wd.findElement(By.name("email")).clear();
+      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+      wd.findElement(By.name("address2")).click();
+      wd.findElement(By.name("address2")).clear();
+      wd.findElement(By.name("address2")).sendKeys(contactData.getAddress2());
+      wd.findElement(By.name("phone2")).click();
+      wd.findElement(By.name("phone2")).clear();
+      wd.findElement(By.name("phone2")).sendKeys(contactData.getPhone2());
+      wd.findElement(By.name("notes")).click();
+  }
+
+  protected void initContactCreation() {
+      wd.findElement(By.linkText("add new")).click();
   }
 }
