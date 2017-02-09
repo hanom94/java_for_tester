695c28bf908f15f28bb9726bf43075527fb838bc
Viktor Yeromenko <hanom94@gmail.com>
2017-02-09 13:54:19 +0200

    Лямбда-выражения (создание групп и контактов)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,6 +6,7 @@ import ru.stqa.pft.addressbook.appmanager.GroupContactHelperBase;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -27,13 +28,7 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getGroupContactHelperBase().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData g : after){
-      if (g.getId() > max){
-        max = g.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
