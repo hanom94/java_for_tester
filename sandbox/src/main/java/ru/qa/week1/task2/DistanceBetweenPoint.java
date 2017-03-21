240ad6cec7b87f05a39298a93e3adfbdac2a3b5c
Viktor Yeromenko <hanom94@gmail.com>
2017-03-21 12:22:55 +0200

    Убраныы комментарии на русском языке

--- sandbox/src/main/java/ru/qa/week1/task2/DistanceBetweenPoint.java
+++ sandbox/src/main/java/ru/qa/week1/task2/DistanceBetweenPoint.java
@@ -6,7 +6,6 @@ public class DistanceBetweenPoint {
     Point p1 = new Point(7, 7);
     Point p2 = new Point(14, 14);
 
-    //Использование функции для вычисления
     System.out.println("Расстояние между двумя точками: "
             + p1.x + " : " + p1.y + " и " + p2.x + " : " + p2.y + " = " + Point.distancePoint(p1,p2));
   }
